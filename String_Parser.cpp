e4358e5e4e1ee0181339670403c29ad84f9186b3
Thomas-Hess <2degreesummit@gmail.com>
2026-03-16 18:51:54 -0700

    Update String_Parser.cpp

--- String_Parser.cpp
+++ String_Parser.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream> // For parsing lines
+#include <sstream> // to parse
 using namespace std;
 
-void parseCSV(const string&);
+void parseCSV(const string& filename);
 
 int main() {
    parseCSV("students.csv");
-
    return 0;
+}
+
+void parseCSV(const string& filename) {
+   ifstream file(filename);
+   string line, token;
+
+   while (getline(file, line)) {
+      stringstream ss(line);
+
+      while (ss >> token) {
+         int pos = token.find(':');
+         cout << token.substr(0, pos) << ": "
+              << token.substr(pos + 1) << endl;
+      }
+      // assuming the format is consistant
+
+      cout << endl; // space between students
+   }
 }
